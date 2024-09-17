4bc16e9cbf638cbfc035bcf263a9bc3d01dbd354
Amani Magdi Farg <amanim.farag@gmail.com>
2024-09-17 13:35:34 +0300

    make get_type() is abstractmethod

--- strategy/message_formatter/message_formatter.py
+++ strategy/message_formatter/message_formatter.py
@@ -4,5 +4,6 @@ class MessageFormatter(ABC):
     @abstractmethod
     def format_message(self, message_to_formate: str) -> str:
         pass
+    @abstractmethod
     def get_type(self) -> str:
         pass
